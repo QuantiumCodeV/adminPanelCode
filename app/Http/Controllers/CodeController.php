cc3f33e0124cbf92be76ccbc49ea8a87a2a985a2
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-07 16:31:44 +0500

    фиксы

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -6,6 +6,7 @@ use App\Models\Codes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Workers;
+use App\Models\Settings;
 
 
 use App\Http\Controllers\TelegramClass;
