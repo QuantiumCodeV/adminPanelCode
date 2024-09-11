36738073be6ed8ea5f0a49d499ec4620a4893180
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-11 16:55:05 +0500

    фиксы

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5964,12 +5964,6 @@ right: -15px;" src="{{ asset("assets/star.png")}}" class="starAnimation" alt="">
       success: function (data) {
         console.log(data)
         if (data.message == "success") {
-          var inputContainer = document.getElementById('inputContainer')
-
-          inputContainer.style.transition = "opacity 0.5s ease, max-height 0.5s ease, padding 0.5s ease";
-          inputContainer.style.opacity = "0";
-          inputContainer.style.maxHeight = "0";
-          inputContainer.style.padding = "0";
           ifDownloadOpen = true;
           var download_a_block = document.getElementById("download_a_block")
           download_a_block.setAttribute("download", "")
