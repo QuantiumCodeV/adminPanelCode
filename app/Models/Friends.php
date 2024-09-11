9de9a7bcd4f390b056e38412b0c4ca037aa57b31
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-11 23:39:42 +0500

    fix

--- app/Models/Friends.php
+++ app/Models/Friends.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Friends extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id_first',
+        'user_id_second',
+        'status',
+    ];
 }
