1f0f76d3e19015d5159fc463a0729f2ed25cb6fe
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-11 17:41:28 +0500

    фиксы

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1006,9 +1006,17 @@ right: -15px;" src="{{ asset("assets/star.png")}}" class="starAnimation" alt="">
                               var sync_a_block =document.getElementById('sync_a_block')
 
                               sync_a_block.style.display = 'flex';
+                              sync_a_block.addEventListener("click", function(){
+                                sync_a_block.classList.add("errorBlock")
+                              })
                             })
                           </script>
-
+<style>
+  .errorBlock{
+    border-color: red !important;
+    color:red !important;
+  }
+</style>
                           <button onclick="showFirstBlock()" style="position:relative;overflow:visible" type="button"
                             class="flex items-center justify-center gap-2 rounded font-medium leading-4 whitespace-nowrap overflow-hidden text-overflow-ellipsis w-auto min-w-0 min-h-0 select-none px-3 py-2 bg-secondary-300 border-[2px] border-neutral-700 shadow-smoothxl opacity-100"
                             tabindex="0" style="transform: none">
