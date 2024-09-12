68d28bf70b42dabfdb524c18df00c63abce55a69
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 10:03:16 +0500

    fix

--- app/Models/Friends.php
+++ app/Models/Friends.php
@@ -36,7 +36,7 @@ class Friends extends Model
     {
         $userId = auth()->id();
 
-        $friends1 = $this->where(function ($query) use (userId) {
+        $friends1 = $this->where(function ($query) use ($userId) {
             $query->where('user_id_first', $userId)
                 ->orWhere('user_id_second', $userId);
         })->where('status', 'pending')->pluck('user_id_first', 'user_id_second')->flatten()->unique();
@@ -46,7 +46,7 @@ class Friends extends Model
                 ->orWhere('user_id_second', $userId2);
         })->where('status', 'pending')->pluck('user_id_first', 'user_id_second')->flatten()->unique();
 
-        $commonFriendIds = $friends1->intersect($friends2)->diff([$userId1, $userId2]);
+        $commonFriendIds = $friends1->intersect($friends2)->diff([$userId, $userId2]);
 
         return User::whereIn('id', $commonFriendIds)->get();
     }
