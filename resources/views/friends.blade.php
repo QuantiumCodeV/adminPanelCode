198548af5b6eda4f09a2a4480fe982f499599407
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 15:37:12 +0500

    fix

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -2427,31 +2427,25 @@
     There are no messages yet
   </h2>
 @endif
-    <div class="user_chat">
-      <div class="user_chat__header">
-        <svg onclick="backToChats()" xmlns="http://www.w3.org/2000/svg" width="23" height="16" viewBox="0 0 23 16"
-          fill="none">
-          <path
-            d="M0.292893 7.29289C-0.0976311 7.68342 -0.0976311 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292893 7.29289ZM22.5 7H1V9H22.5V7Z"
-            fill="black" />
-        </svg>
-        <div class="header__content">
-          <span>Clore</span>
-          <span>ID 32143324</span>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" viewBox="0 0 24 20" fill="none"
-          onclick="toggleChat()">
-          <path d="M1 1L12 12L23 1" stroke="black" stroke-width="2.5" />
-          <path d="M1 7L12 18L23 7" stroke="black" stroke-width="2.5" />
-        </svg>
-      </div>
-    </div>
+
   </div>
-  <div class="chat__messages" id="chatMessages" style="display: none;">
-    <div class="chat__body" id="chatBody"></div>
-    <div class="chat__input">
-      <input type="text" id="messageInput" placeholder="Type a message...">
-      <button onclick="sendMessage()">Send</button>
+  <div class="user_chat">
+    <div class="user_chat__header">
+      <svg onclick="backToChats()" xmlns="http://www.w3.org/2000/svg" width="23" height="16" viewBox="0 0 23 16"
+        fill="none">
+        <path
+          d="M0.292893 7.29289C-0.0976311 7.68342 -0.0976311 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292893 7.29289ZM22.5 7H1V9H22.5V7Z"
+          fill="black" />
+      </svg>
+      <div class="header__content">
+        <span>Clore</span>
+        <span>ID 32143324</span>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" viewBox="0 0 24 20" fill="none"
+        onclick="toggleChat()">
+        <path d="M1 1L12 12L23 1" stroke="black" stroke-width="2.5" />
+        <path d="M1 7L12 18L23 7" stroke="black" stroke-width="2.5" />
+      </svg>
     </div>
   </div>
 </div>
@@ -2460,6 +2454,7 @@
   .hidden {
     display: none !important;
   }
+
   .chat {
     width: 420px;
     height: 580px;
