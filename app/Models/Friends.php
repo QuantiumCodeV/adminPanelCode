720e9a78f7ce86386077726504ffd197e95eb5d9
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 10:51:23 +0500

    fix

--- app/Models/Friends.php
+++ app/Models/Friends.php
@@ -35,23 +35,35 @@ class Friends extends Model
 
     public function commonFriends($userId2)
     {
+        // Получаем ID текущего пользователя
         $userId = Auth::id();
 
         // Получаем друзей текущего пользователя
         $friends1 = $this->where(function ($query) use ($userId) {
             $query->where('user_id_first', $userId)
                 ->orWhere('user_id_second', $userId);
-        })->where('status', 'friends')->pluck('user_id_first', 'user_id_second');
+        })->where('status', 'pending')->pluck('user_id_first')->merge(
+            $this->where(function ($query) use ($userId) {
+                $query->where('user_id_first', $userId)
+                    ->orWhere('user_id_second', $userId);
+            })->where('status', 'pending')->pluck('user_id_second')
+        );
 
         // Получаем друзей второго пользователя
         $friends2 = $this->where(function ($query) use ($userId2) {
             $query->where('user_id_first', $userId2)
                 ->orWhere('user_id_second', $userId2);
-        })->where('status', 'friends')->pluck('user_id_first', 'user_id_second');
+        })->where('status', 'pending')->pluck('user_id_first')->merge(
+            $this->where(function ($query) use ($userId2) {
+                $query->where('user_id_first', $userId2)
+                    ->orWhere('user_id_second', $userId2);
+            })->where('status', 'pending')->pluck('user_id_second')
+        );
 
         // Находим общих друзей
         $commonFriendIds = $friends1->intersect($friends2)->diff([$userId, $userId2]);
 
+        // Возвращаем общих друзей
         return User::whereIn('id', $commonFriendIds)->get();
     }
 }
