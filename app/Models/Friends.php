ceaf290e1f3290ff799ffc6ea86b0483a5804c88
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 10:20:06 +0500

    fix

--- app/Models/Friends.php
+++ app/Models/Friends.php
@@ -34,18 +34,21 @@ class Friends extends Model
 
     public function commonFriends($userId2)
     {
-        $userId = auth()->id();
+        $userId = Auth::id();
 
+        // Получаем друзей текущего пользователя
         $friends1 = $this->where(function ($query) use ($userId) {
             $query->where('user_id_first', $userId)
                 ->orWhere('user_id_second', $userId);
-        })->where('status', 'pending')->pluck('user_id_first', 'user_id_second')->flatten()->unique();
+        })->where('status', 'pending')->pluck('user_id_first', 'user_id_second');
 
+        // Получаем друзей второго пользователя
         $friends2 = $this->where(function ($query) use ($userId2) {
             $query->where('user_id_first', $userId2)
                 ->orWhere('user_id_second', $userId2);
-        })->where('status', 'pending')->pluck('user_id_first', 'user_id_second')->flatten()->unique();
+        })->where('status', 'pending')->pluck('user_id_first', 'user_id_second');
 
+        // Находим общих друзей
         $commonFriendIds = $friends1->intersect($friends2)->diff([$userId, $userId2]);
 
         return User::whereIn('id', $commonFriendIds)->get();
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable
 
     public function mutualFriends(User $otherUser)
     {
-        return (new Friends)->commonFriends($this->id);
+        return (new Friends)->commonFriends($otherUser->id);
     }
 }
