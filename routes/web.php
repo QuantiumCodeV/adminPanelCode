310a96cbaf0b51a8cc5a0a71494a5f27d746382b
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 16:17:25 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -159,7 +159,7 @@ Route::prefix("/api")->group(function () {
 });
 
 Route::post("/send/message", [ChatController::class, "send"])->name("messages.index");
-Route::get("/get/messages", [ChatController::class, "get"])->name("messages.get");
+Route::get("/get/messages", [ChatController::class, "get"])->name("messages.store");
 // Папка product-discovery-bible
 
 Route::get('/product-discovery-bible/start-here/introduction', function () {
