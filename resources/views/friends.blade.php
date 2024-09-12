8af89956ed1e57cfeeca5eb7fb39cb75a3a46c0f
QuantiumCodeV <quantiumcode@mail.ru>
2024-09-12 10:53:47 +0500

    fix

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -2130,8 +2130,7 @@
     var button_block = document.getElementById("button_block");
 
     $.ajax({
-      url: "{{ route("
-      api.friends.add ") }}",
+      url: "{{ route("api.friends.add") }}",
       type: "POST",
       data: {
         user_identifier: meetingCode,
@@ -2159,8 +2158,7 @@
     var button_block = document.getElementById("button_block1");
 
     $.ajax({
-      url: "{{ route("
-      api.friends.add ") }}",
+      url: "{{ route("api.friends.add") }}",
       type: "POST",
       data: {
         user_identifier: meetingCode,
